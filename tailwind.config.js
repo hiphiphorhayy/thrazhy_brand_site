a521472f0bbd00d0a7c1b23de5103bbbd3fd6c10
George <mgeor379@outlook.com>
2024-12-01 17:39:18 +0800

    Add footer & contact page section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html'],
+  content: ['./public/index.html', './public/merch.html', './public/vino.html', './public/extras.html', './public/contacto.html', './public/cart.html'],
   theme: {
     extend: {},
   },
